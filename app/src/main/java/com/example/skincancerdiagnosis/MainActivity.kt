c0035a7920bf4d6fa09d21dd51f5427deaac8208
Arif Mertaslan <arif.mertaslan@ibtech.com.tr>
2022-04-17 20:51:38 +0300

    camera and gallery integration completed

--- app/src/main/java/com/example/skincancerdiagnosis/MainActivity.kt
+++ app/src/main/java/com/example/skincancerdiagnosis/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.skincancerdiagnosis
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.skincancerdiagnosis.databinding.ActivityMainBinding
+import java.io.FileNotFoundException
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val GET_FROM_CAMERA = 1
+        const val GET_FROM_GALLERY = 2
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -15,7 +27,42 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.openCameraCard.setOnClickListener {
-            Toast.makeText(this, "Olduuuug", Toast.LENGTH_LONG).show()
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, GET_FROM_CAMERA)
+        }
+
+        binding.openGalleryCard.setOnClickListener {
+            startActivityForResult(
+                Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.INTERNAL_CONTENT_URI
+                ),
+                GET_FROM_GALLERY
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                GET_FROM_GALLERY -> {
+                    val selectedImage: Uri = data?.data!!
+                    var bitmap: Bitmap? = null
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+                        binding.galleryIconImageview.setImageBitmap(bitmap)
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+                GET_FROM_CAMERA -> {
+                    val thumbnail = data!!.extras!!.get("data") as Bitmap
+                    binding.cameraIconImageview.setImageBitmap(thumbnail)
+                }
+            }
         }
     }
 }
