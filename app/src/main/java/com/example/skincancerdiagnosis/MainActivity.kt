f5fbb6d814d081d89fe7ad175eb2a131553d2f46
Arif Mertaslan <arif.mertaslan@ibtech.com.tr>
2022-05-06 20:37:29 +0300

    adding crop page for camera

--- app/src/main/java/com/example/skincancerdiagnosis/MainActivity.kt
+++ app/src/main/java/com/example/skincancerdiagnosis/MainActivity.kt
@@ -1,26 +1,30 @@
 package com.example.skincancerdiagnosis
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import com.example.skincancerdiagnosis.databinding.ActivityMainBinding
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val GET_FROM_CAMERA = 1
-        const val GET_FROM_GALLERY = 2
-        const val IMAGE_SIZE_TO_TEST = 224
-        const val IMAGE_SIZE_TO_SHOW = 896
+        const val GET_FROM_CAMERA = 10
+        const val GET_FROM_GALLERY = 20
     }
 
     private lateinit var thumbnail: Bitmap
@@ -30,15 +34,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         initializeListeners()
+        enableRuntimePermission()
     }
 
     private fun initializeListeners() {
         binding.openCameraCard.setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            val file = File(this.externalCacheDir, System.currentTimeMillis().toString() + ".jpg")
-            val uri = Uri.fromFile(file)
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-            startActivityForResult(intent, GET_FROM_CAMERA)
+            val cameraImgIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            cameraImgIntent.putExtra(
+                MediaStore.EXTRA_OUTPUT,
+                FileProvider.getUriForFile(
+                    Objects.requireNonNull(applicationContext),
+                    BuildConfig.APPLICATION_ID + ".provider",
+                    File(getExternalFilesDir(null), "camera_image.jpg")
+                )
+            )
+            startActivityForResult(cameraImgIntent, GET_FROM_CAMERA)
         }
 
         binding.openGalleryCard.setOnClickListener {
@@ -51,7 +61,7 @@ class MainActivity : AppCompatActivity() {
         if (resultCode == RESULT_OK) {
             when (requestCode) {
                 GET_FROM_GALLERY -> { getCroppedImage(data) }
-                GET_FROM_CAMERA -> { getCroppedImage(data) }
+                GET_FROM_CAMERA -> { getCroppedImage(data?.extras?.getParcelable(MediaStore.EXTRA_OUTPUT)) }
                 CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> { runOnCropImage(data) }
             }
         }
@@ -60,7 +70,8 @@ class MainActivity : AppCompatActivity() {
     private fun runOnCropImage(data: Intent?) {
         try {
             thumbnail = MediaStore.Images.Media.getBitmap(this.contentResolver, data?.data!!)
-            goToResultActivity(createImageByteArray(thumbnail))
+            binding.cameraIconImageview.setImageBitmap(thumbnail)
+            //goToResultActivity(createImageByteArray(thumbnail))
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -129,5 +140,34 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("imageToShow", imageToShow)
         startActivity(intent)
         */
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            GET_FROM_CAMERA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun enableRuntimePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Toast.makeText(
+                this,
+                "Camera permission needed. Please allow in App Settings for additional functionality.",
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), GET_FROM_CAMERA)
+        }
     }
 }
